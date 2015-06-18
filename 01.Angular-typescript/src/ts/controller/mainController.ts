b67a02b54341dab4eeac6c0a08788e4361c5168e
yasuaki-hikishima <yasuaki_hikishima@waku-2.com>
2015-06-18 21:57:36 +0900

    once delete

--- 01.Angular-typescript/src/ts/controller/mainController.ts
+++ 01.Angular-typescript/src/ts/controller/mainController.ts
@@ -1,8 +1,2 @@
 // mainController.ts
 /// <reference path="../../typings/tsd.d.ts" />
-
-export interface MinScope extends ng.IScope {
-  content: string;
-  items: app.models.Item[];
-  add(item: string): void;
-}
